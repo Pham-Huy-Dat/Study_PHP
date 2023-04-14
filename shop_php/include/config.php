16943b43dfb0685ba4044d4d8fdb39ce9abe97af
Henry-Pham <101860656+Pham-Huy-Dat@users.noreply.github.com>
2023-04-14 20:42:02 +0700

    update

--- shop_php/include/config.php
+++ shop_php/include/config.php
@@ -5,13 +5,13 @@
  */
 
 // Thư mục gốc ở htdocs (đối với XAMPP)
-define('ROOT_PATH', "/project-eShop_php");
+define('ROOT_PATH', "/shop_php");
 
 // Thư mục chứa file (css/js/img)
-define('ASSET_PATH', "/project-eShop_php/assets");
+define('ASSET_PATH', "/shop_php/assets");
 
 // Thư mục chứa file upload bởi user
-define('UPLOAD_PATH', "/project-eShop_php/upload");
+define('UPLOAD_PATH', "/shop_php/upload");
 
 // Đường dẫn đầy đủ đến thư mục hiện tại, không cần chỉnh sửa nếu dùng XAMPP
 define('DOCUMENT_ROOT_PATH', $_SERVER["DOCUMENT_ROOT"]);
